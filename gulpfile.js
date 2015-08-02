3f5a02cae2ff4ea6b000da4d801e31ead64e9fdb
Todd Treece <toddtreece@gmail.com>
2015-08-01 20:04:08 -0400

    adds travis

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ gulp.task('test', function() {
   return gulp.src('test/*.js', {read: false})
     .pipe(mocha())
     .once('error', function(err) {
-      console.error(err);
+      console.error(err.toString());
       process.exit(1);
     })
     .once('end', function() {
