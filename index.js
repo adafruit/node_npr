74faf248019cd726ad84807e235f0adff0801d6a
Todd Treece <toddtreece@gmail.com>
2015-08-21 21:46:26 +0000

    don't enforce one auth grant_type. allow user to pass access token

--- index.js
+++ index.js
@@ -16,29 +16,11 @@ function NPR(config) {
 
   util._extend(this, config || {});
 
-  if (! this.client_id)
-    throw('OAuth Client ID not supplied');
-
-  if (! this.client_secret)
-    throw('OAuth Client Secret not supplied');
-
-  if (! this.username)
-    throw('NPR username not supplied');
-
-  if (! this.password)
-    throw('NPR password not supplied');
-
   if (! this.log)
     this.log = bunyan.createLogger({name: 'npr'});
 
   this.one = One({
-    log: this.log.child({ module: 'one' }),
-    credentials: {
-      client_id: this.client_id,
-      client_secret: this.client_secret,
-      username: this.username,
-      password: this.password
-    }
+    log: this.log.child({ module: 'one' })
   });
 
 }
@@ -48,9 +30,5 @@ NPR.Helpers = Helpers;
 NPR.One = One;
 
 /*************************** DEFAULTS *****************************/
-proto.client_id = false;
-proto.client_secret = false;
-proto.username = false;
-proto.password = false;
 proto.one = false;
 proto.log = false;
